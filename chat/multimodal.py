8aca25fb56d39325cc7077ee23155ee0d0e66dc0
tsilva <eng.tiago.silva@gmail.com>
2025-01-16 17:25:56 +0000

    Add multimodal chat interface and multiple input handling with Gradio

---
+++ chat/multimodal.py
@@ -0,0 +1,21 @@
+import gradio as gr
+
+def count_images(message, history):
+    num_images = len(message["files"])
+    total_images = 0
+    for message in history:
+        if isinstance(message["content"], tuple):
+            total_images += 1
+    return f"You just uploaded {num_images} images, total uploaded: {total_images+num_images}"
+
+demo = gr.ChatInterface(
+    fn=count_images, 
+    type="messages", 
+    examples=[
+        {"text": "No files", "files": []}
+    ], 
+    multimodal=True,
+    textbox=gr.MultimodalTextbox(file_count="multiple", file_types=["image"], sources=["upload", "microphone"])
+)
+
+demo.launch()
